cb4e6d8e1309af8ab028ccb26fa0b65c87dcdb8c
fahrenq <git@ivan.engineering>
2022-12-13 19:45:48 +0100

    Day 8 part 2

--- day-8/Day8.fsx
+++ day-8/Day8.fsx
@@ -10,9 +10,6 @@ let TEST_INPUT =
 33549
 35390"""
 
-let merge (left: bool[]) (right: bool[]) =
-  left |> Array.map2 (fun l r -> l || r) right
-
 let getMatrix (input: string) =
   input.Split("\n", StringSplitOptions.RemoveEmptyEntries)
   |> Array.map (fun r -> r.ToCharArray() |> Array.map (string >> Int32.Parse))
@@ -24,8 +21,7 @@ module Part1 =
   let visibleBothDirections row =
     let d1 = visibleOneDirection row
     let d2 = visibleOneDirection (Array.rev row) |> Array.rev
-    merge d1 d2
-
+    Array.map2 (||) d1 d2
 
   let solve (input: string) =
     let m = getMatrix input
@@ -34,9 +30,56 @@ module Part1 =
     let visibleVertical =
       m |> Array.transpose |> Array.map visibleBothDirections |> Array.transpose
 
-    let visibilityMap = Array.map2 merge visibleHorizontal visibleVertical
+    let visibilityMap =
+      Array.map2 (Array.map2 (||)) visibleHorizontal visibleVertical
 
     visibilityMap |> Array.collect id |> Array.filter id |> Array.length
 
 module Part2 =
-  let solve (input: string) = -1
+  let visibleTrees startingValue (row: int[]) =
+    row
+    |> Array.mapFold
+      (fun blocked x -> not blocked, blocked || (x >= startingValue))
+      false
+    |> fst
+
+  let scoresOneRow (row: int[]) =
+    row
+    |> Array.mapi (fun i x ->
+      let toTheLeft =
+        row[0 .. i - 1]
+        |> Array.rev
+        |> visibleTrees x
+        |> Array.filter id
+        |> Array.length
+
+      let toTheRight =
+        row[i + 1 ..] |> visibleTrees x |> Array.filter id |> Array.length
+
+      toTheLeft * toTheRight
+    )
+
+  let prettyPrintMatrix (m: int[][]) =
+    m
+    |> Array.map (fun row -> row |> Array.map string |> String.concat " ")
+    |> String.concat "\n"
+    |> printfn "%s"
+
+  let solve (input: string) =
+    let m = getMatrix input
+    let scoresHorizontal = m |> Array.map scoresOneRow
+
+    let scoresVertical =
+      m |> Array.transpose |> Array.map scoresOneRow |> Array.transpose
+
+    let scoresMap = Array.map2 (Array.map2 (*)) scoresHorizontal scoresVertical
+
+    scoresMap |> Array.collect id |> Array.max
+
+let s1 = Part1.solve INPUT
+let s2 = Part2.solve INPUT
+
+(*
+I really need to stop attempting to fix solution by trying random things. It sucks my energy out.
+I should have just take it slow, not panic and solve it piece by piece, checking for all edge cases.
+*)
